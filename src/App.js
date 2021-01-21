c4196c591808be3fd4caf0a3d33e97b5da7f4c0d
Rahul Dhiman <rdrahuldhiman.19@gmail.com>
2021-01-21 23:09:35 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi React</h1>
+      <h2>
+        i know everyThing is working fine but still making sure that everything
+        is fine..im repetetive
+      </h2>
     </div>
   );
 }
